5611920b220760bc16fe07f14c22613295e7730e
Zhanar Mazhit <noturfbs@gmail.com>
2024-11-04 14:23:46 +0500

    Correcting the code

--- task1/problem1.py
+++ task1/problem1.py
@@ -1,14 +1,18 @@
-class Solution:
-    def findLengthOfLCIS(self, nums: List[int]) -> int:
-        if len(nums) == 1:
-            return 1
-        else:
-            cur_length = 1
-            max_length = 1
-            for i in range(len(nums)-1):
-                if nums[i] < nums[i+1]:
-                    cur_length+=1
-                else:
-                    max_length = cur_length
-                    cur_length=1
-        return max_length
+class Solution(object):
+    def findLengthOfLCIS(self, nums):
+        if not nums:
+            return 0
+        
+        cur_length = 1
+        max_length = 1
+
+        for i in range(len(nums)-1):
+            if nums[i] < nums[i+1]:
+                cur_length+=1
+            else:
+                max_length = max(max_length, cur_length)
+                cur_length=1
+        return max(max_length, cur_length)
+    
+solution = Solution()
+print(solution.findLengthOfLCIS([1, 3, 5, 4, 7, 8, 9]))
